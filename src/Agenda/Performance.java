30769880bd5c5a051a6d1d5a3bafff59ee544d2e
maartenks <mjjkesse@avans.nl>
2019-02-04 11:55:22 +0100

    performance

--- src/Agenda/Performance.java
+++ src/Agenda/Performance.java
@@ -1,4 +1,46 @@
 package Agenda;
 
+import java.time.LocalTime;
+import java.util.ArrayList;
+
 public class Performance {
+
+    private ArrayList<Artist> artists;
+    private LocalTime beginTime;
+    private LocalTime endTime;
+
+    public Performance(ArrayList<Artist> artists, LocalTime beginTime, LocalTime endTime){
+        this.artists = artists;
+        this.beginTime = beginTime;
+        this.endTime = endTime;
+
+    }
+
+    public void addArtist(Artist artist) {
+        this.artists.add(artist);
+    }
+
+    public void setArtists(ArrayList<Artist> artists) {
+        this.artists = artists;
+    }
+
+    public void setBeginTime(LocalTime beginTime) {
+        this.beginTime = beginTime;
+    }
+
+    public void setEndTime(LocalTime endTime) {
+        this.endTime = endTime;
+    }
+
+    public ArrayList<Artist> getArtists() {
+        return artists;
+    }
+
+    public LocalTime getBeginTime() {
+        return beginTime;
+    }
+
+    public LocalTime getEndTime() {
+        return endTime;
+    }
 }
