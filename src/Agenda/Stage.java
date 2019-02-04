74632c4f35019b7153a61ab93341b1027d67107b
EricVeen <ericveen@hotmail.nl>
2019-02-04 13:22:45 +0100

    test

--- src/Agenda/Stage.java
+++ src/Agenda/Stage.java
@@ -4,14 +4,16 @@ import java.time.LocalTime;
 import java.util.ArrayList;
 
 public class Stage {
+    private Performance performances;
     private int maxVisitors;
 
-    Stage(int maxVisitors) {
+   public Stage(Performance performances,int maxVisitors) {
+        this.performances = performances;
         this.maxVisitors = maxVisitors;
     }
 
-    public void addPerfomance(LocalTime beginTime, LocalTime endTime) {
-        Performance performance = new Performance(beginTime, endTime);
+    public void addPerfomance(Performance performance) {
+       // performances.add(performance);
     }
 
     public void setMaxVisitors(int maxVisitors) {
@@ -19,14 +21,14 @@ public class Stage {
     }
 
     public void setPerformances(ArrayList<Performance> performances) {
-        this.performances = performances;
+      //  this.performances = performances;
     }
 
     public int getMaxBezoekers() {
         return maxVisitors;
     }
 
-    public ArrayList <Performance> getPerformances() {
-        return performances;
-    }
+//    public ArrayList<Performance> getPerformances() {
+//      //  return performances;
+//    }
 }
