3e0e5b611abb8c841205898c9fe717dd743b274c
Chiem <Chiem.stevens@outlook.com>
2019-02-11 14:19:31 +0100

    Save and read objects
    Added serializer class, added some main logic to test.

--- src/Main.java
+++ src/Main.java
@@ -7,23 +7,16 @@ import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
-import java.lang.reflect.Array;
-import java.sql.Time;
 import java.time.LocalTime;
 import java.util.ArrayList;
 
 public class Main {
 
     public static void main(String [ ] args) {
-//        Artist neffex = new Artist("Neffex", 32);
-//        Artist lilwayne = new Artist("Lil Wayne", 52);
-//        LocalTime time = LocalTime.of(12,0);
-//        Performance performance = new Performance(neffex, time, time );
-//        Stage stage = new Stage(performance, 200);
-//        Timetable timetable = new Timetable(stage);
-//        System.out.println(timetable.getStages().getPerformances().getArtists().getName());
 
-        ArrayList<Stage> stages = new ArrayList<Stage>();
+        /*Serializer serializer = new Serializer();
+
+        ArrayList<Stage> stages = new ArrayList<>();
         ArrayList<Performance> performances = new ArrayList<>();
         ArrayList<Artist> artists = new ArrayList<>();
 
@@ -34,23 +27,12 @@ public class Main {
         stages.add(new Stage(performances, 200));
         Timetable timeTable = new Timetable(stages);
 
-        try {
-            ObjectOutputStream objectOutputStream = new ObjectOutputStream(new FileOutputStream("data/person.bin"));
-            objectOutputStream.writeObject(timeTable);
-            objectOutputStream.close();
-        } catch(Exception ex) {
-            System.out.println(ex);
-        }
+        serializer.setTimeTable(timeTable);
+        serializer.serializeTimeTable("TimeTable1");*/
 
-        try {
-            ObjectInputStream objectInputStream = new ObjectInputStream(new FileInputStream("data/person.bin"));
-            Timetable readTimeTable = (Timetable) objectInputStream.readObject();
-            System.out.println(readTimeTable);
-            objectInputStream.close();
-        } catch(Exception ex) {
-            System.out.println(ex);
-        }
+
+
+
 
     }
-
 }
